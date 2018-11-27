ffd77eda0f3cd62c3e2f704da8a45931ca2b23c7
jackindisguise <incognitus.me@gmail.com>
2018-11-27 02:50:38 -0600

    net test now uses package version for things

--- unit-tests/src/net.js
+++ unit-tests/src/net.js
@@ -1,3 +1,6 @@
+// node includes
+var util = require("util");
+
 // npm includes
 var expect = require("chai").expect;
 var http = require("http");
@@ -7,6 +10,7 @@ var io = require("socket.io-client");
 var MUD = require("../../src/core/MUD");
 var Database = require("../../src/core/Database");
 var _ = require("../../i18n");
+var _package = require("../../package.json");
 
 describe("Net", function(){
     describe("Start", function(){
@@ -26,7 +30,7 @@ describe("Net", function(){
                 });
 
                 res.on("end", function(){
-                    expect(raw).to.equal('<!DOCTYPE html>\n<html>\n  <head>\n    <title>Node Page</title>\n    <link rel="stylesheet" type="text/css" href="/css/main.css">\n    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">\n    <link rel="icon" href="/favicon.ico" type="image/x-icon">\n    <link href="https://fonts.googleapis.com/css?family=Fira+Mono|Source+Code+Pro" rel="stylesheet">\n    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>\n    <script src="/socket.io/socket.io.js"></script>\n    <script src="/src/js/socket.js"></script>\n    <script src="/src/js/output.js"></script>\n    <script src="/src/js/input.js"></script>\n    <script src="/src/js/main.js"></script>\n  </head>\n  <body>\n    <div id="header">mudengine\n      \n    </div>\n    <div id="MUD">\n      <div id="output"></div>\n      <div id="input">\n        <div class="wrapper">\n          <input id="command" placeholder="...">\n        </div>\n      </div>\n    </div>\n    <div id="status">mudengine v0.0.0</div>\n  </body>\n</html>');
+                    expect(raw).to.equal(util.format('<!DOCTYPE html>\n<html>\n  <head>\n    <title>Node Page</title>\n    <link rel="stylesheet" type="text/css" href="/css/main.css">\n    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">\n    <link rel="icon" href="/favicon.ico" type="image/x-icon">\n    <link href="https://fonts.googleapis.com/css?family=Fira+Mono|Source+Code+Pro" rel="stylesheet">\n    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>\n    <script src="/socket.io/socket.io.js"></script>\n    <script src="/src/js/socket.js"></script>\n    <script src="/src/js/output.js"></script>\n    <script src="/src/js/input.js"></script>\n    <script src="/src/js/main.js"></script>\n  </head>\n  <body>\n    <div id="header">mudengine\n      \n    </div>\n    <div id="MUD">\n      <div id="output"></div>\n      <div id="input">\n        <div class="wrapper">\n          <input id="command" placeholder="...">\n        </div>\n      </div>\n    </div>\n    <div id="status">mudengine v%s</div>\n  </body>\n</html>', _package.version));
                     done();
                 });
             });
