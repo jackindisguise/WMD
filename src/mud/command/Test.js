a74a1ca6ab48b73b1b068f1bb57175c47fc4d89c
jackindisguise <incognitus.me@gmail.com>
2019-01-07 12:22:43 -0600

    Garbage collection seems to be working properly for the map and map objects.
    Seems to be working for player mobs as well.
    
    Pretty happy about it.

--- src/mud/command/Test.js
+++ src/mud/command/Test.js
@@ -1,13 +1,38 @@
+// node includes
+const process = require("process");
+
 // local includes
 const _ = require("../../../i18n");
 const Command = require("../Command");
 const CommandSpecificity = require("../CommandSpecificity");
+const ObjectManager = require("../manager/ObjectManager");
 
 class Test extends Command{
 	exec(mob){
-		let txt = JSON.stringify(mob.__toJSON(), null, "\t");
-		txt = txt.replace("{", "{{");
-		mob.sendLine(txt);
+		let Mob = require("../map/Mob");
+		console.log("before:", process.memoryUsage());
+		let count = 10000;
+		for(var c=0;c<count;c++){
+			let m = new Mob();
+			m.loc = mob.loc;
+			setTimeout(function(){
+				m.loc = null;
+			}, 1000);
+		}
+
+		console.log(`after creating ${count} mobs:`, process.memoryUsage());
+		setTimeout(function(){
+			console.log("2 seconds after moving to the void:", process.memoryUsage());
+		}, 3000);
+		setTimeout(function(){
+			console.log("10 seconds after moving to the void:", process.memoryUsage());
+		}, 10000);
+		setTimeout(function(){
+			console.log("20 seconds after moving to the void:", process.memoryUsage());
+		}, 20000);
+		setTimeout(function(){
+			console.log("30 seconds after moving to the void:", process.memoryUsage());
+		}, 30000);
 	}
 }
 
