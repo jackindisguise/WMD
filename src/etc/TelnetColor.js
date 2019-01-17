02bb955616ffa2838f5203d1101893f40486f526
jackindisguise <incognitus.me@gmail.com>
2019-01-16 19:07:20 -0600

    ???

---
+++ src/etc/TelnetColor.js
@@ -0,0 +1,21 @@
+const TelnetColor = {
+	BLINK:			"\u001b[5m",
+	CLEAR:			"\u001b[0m",
+	C_RED:			"\u001b[0;31m",
+	C_GREEN:		"\u001b[0;32m",
+	C_YELLOW:		"\u001b[0;33m",
+	C_BLUE:			"\u001b[0;34m",
+	C_MAGENTA:		"\u001b[0;35m",
+	C_CYAN:			"\u001b[0;36m",
+	C_WHITE:		"\u001b[0;37m",
+	C_D_GREY:		"\u001b[1;30m",
+	C_B_RED:		"\u001b[1;31m",
+	C_B_GREEN:		"\u001b[1;32m",
+	C_B_YELLOW:		"\u001b[1;33m",
+	C_B_BLUE:		"\u001b[1;34m",
+	C_B_MAGENTA:	"\u001b[1;35m",
+	C_B_CYAN:		"\u001b[1;36m",
+	C_B_WHITE:		"\u001b[1;37m",
+};
+
+module.exports = TelnetColor;
