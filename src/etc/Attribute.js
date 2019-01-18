ca2f56af93dcd5b0409bc11031d47553c4755683
jackindisguise <incognitus.me@gmail.com>
2019-01-18 06:52:55 -0600

    Fixed JSDoc not accounting for /src/etc.
    PS: I need to standardize how I want to expose static data like enums.
    As it stands, I think I have a few files where I use namespaces and others
    where I use classes.
    
    Annoying.

--- src/etc/Attribute.js
+++ src/etc/Attribute.js
@@ -1,47 +1,51 @@
-const attribute = {
-	STRENGTH: "strength",
-	ATTACK_POWER: "attackPower",
-	DEFENSE: "defense",
-	VITALITY: "vitality",
-	AGILITY: "agility",
-	PRECISION: "precision",
-	DEFLECTION: "deflection",
-	STAMINA: "stamina",
-	INTELLIGENCE: "intelligence",
-	MAGIC_POWER: "magicPower",
-	RESILIENCE: "resilience",
-	WISDOM: "wisdom",
-	HEALTH: "health",
-	ENERGY: "energy",
-	MANA: "mana"
+/**
+ * Exposes values for assisting with accessing attributes.
+ * @namespace
+ */
+const Attribute = {
+	/**
+	 * String literals for accessing attributes directly.
+	 * @enum {string}
+	 */
+	attribute: {
+		STRENGTH: "strength",
+		ATTACK_POWER: "attackPower",
+		DEFENSE: "defense",
+		VITALITY: "vitality",
+		AGILITY: "agility",
+		PRECISION: "precision",
+		DEFLECTION: "deflection",
+		STAMINA: "stamina",
+		INTELLIGENCE: "intelligence",
+		MAGIC_POWER: "magicPower",
+		RESILIENCE: "resilience",
+		WISDOM: "wisdom",
+		HEALTH: "health",
+		ENERGY: "energy",
+		MANA: "mana"
+	},
+
+	/**
+	 * Sanitized string literals for displaying attributes to users.
+	 * @enum {string}
+	 */
+	display: {
+		STRENGTH: "strength",
+		ATTACK_POWER: "attack power",
+		DEFENSE: "defense",
+		VITALITY: "vitality",
+		AGILITY: "agility",
+		PRECISION: "precision",
+		DEFLECTION: "deflection",
+		STAMINA: "stamina",
+		INTELLIGENCE: "intelligence",
+		MAGIC_POWER: "magic power",
+		RESILIENCE: "resilience",
+		WISDOM: "wisdom",
+		HEALTH: "health",
+		ENERGY: "energy",
+		MANA: "mana"
+	}
 };
 
-const display = {
-	STRENGTH: "strength",
-	ATTACK_POWER: "attack power",
-	DEFENSE: "defense",
-	VITALITY: "vitality",
-	AGILITY: "agility",
-	PRECISION: "precision",
-	DEFLECTION: "deflection",
-	STAMINA: "stamina",
-	INTELLIGENCE: "intelligence",
-	MAGIC_POWER: "magic power",
-	RESILIENCE: "resilience",
-	WISDOM: "wisdom",
-	HEALTH: "health",
-	ENERGY: "energy",
-	MANA: "mana"
-};
-
-class Attributes{
-	static get attribute(){
-		return attribute;
-	}
-
-	static get display(){
-		return display;
-	}
-}
-
-module.exports = Attributes;
+module.exports = Attribute;
